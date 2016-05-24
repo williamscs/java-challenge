b1133562028353629f8cdb52022e3baba9cf7731
Chris Williams <cwilliams88@gmail.com>
2016-05-23 21:24:32 -0500

    initial commit

---
+++ src/main/java/com/williamschris/JavaChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.williamschris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallengeApplication.class, args);
+	}
+}
